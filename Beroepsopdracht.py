515a58def8936b12b19995427b20130ee3ba70dc
Cedricaf <34737@ma-web.nl>
2022-10-21 00:23:09 +0200

    Beroepsopdracht.py

--- Beroepsopdracht.py
+++ Beroepsopdracht.py
@@ -20,11 +20,27 @@ while True:
             answer = input("========================================================================\nJe vermomd je zo snel mogelijk en omdat jij een filmacteur was, werkt jouw vermomming zo goed dat de politie je niet herkend en weer vertrekt.\nJe pakt je spullen en gaat naar het vliegveld.\nDaar aangekomen zie je dat er politie staat bij de duane\nJe besluit om…\nA. Op de vermomming te rekenen. Dus niks en normaal gedragen.\nB. Proberen om te duane te ontwijken.")
             if answer == "A":
                 time.sleep(1)
-                answer = input("========================================================================\nnice!")
+                answer = input("========================================================================\nJe denkt dat de vermomming goed werkt en je komt zonder problemen door de duane.\nNa 3uur wachten, vertrekt jouw vliegtuig naar Schiphol.\nAangekomen in schiphol kom je langs een café en je neemt…\nA.Niks.\nB. Koffie.\nThee.")
+                if answer == "A":
+                    time.sleep(1)
+                    answer = input("========================================================================\n")
+                elif answer == "B":
+                    time.sleep(1)
+                    answer == input("========================================================================\n")
+                elif answer == "C":
+                    time.sleep(1)
+                    print("========================================================================\nJe neemt de thee, maar de thee blijkt vergiftigd te zijn.\nHelaas ben je niet in staat om verder te gaan en moet je naar het ziekenhuis.\n[EINDE]")
+                    break
+                else:
+                    print("Dat is geen optie!")
+                    break
             elif answer == "B":
                 time.sleep(1)
                 print("========================================================================\nJe probeert de duane te ontwijken, maar camera's zien dat je heel verdacht doet en de politie wordt ingelicht.\nJe wordt helaas opgepakt en naar de gevangenis gebracht.\n[Einde]")
                 break
+        else:
+            print("Dat is geen optie!")
+            break
     else:
         print("je moet het goede intypen!")
         break
